4ce192148232eb3aa4306aae1a404dfe1a3900d1
DerrickDK <derrickdkyei@yahoo.com>
2014-10-10 23:01:18 -0400

    Create script.js

---
+++ HTML-CSS-JS/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.nav a').mouseenter(function() {
+     $('.nav a').fadeTo('fast', 1); 
+  });
+$(document).ready(function() {
+  $('.nav a').mouseleave(function() {
+     $('.nav a').fadeTo('fast', 0.5); 
+  });
+});
+    
+});
+    
